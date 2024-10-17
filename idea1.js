bfd1c6c00447dde8180beb7bff423af711d3aa8f
Ivan Kucher <dev.kucher@gmail.com>
2024-10-17 20:48:13 +0300

    init

---
+++ idea1.js
@@ -0,0 +1,1 @@
+let increaseThisCounterEveryTimeYouThinkWhatTheFuckIsThisOrRandomDoesntWork = 134 * Date.now();console.log(Math.floor(((1103515245 * increaseThisCounterEveryTimeYouThinkWhatTheFuckIsThisOrRandomDoesntWork + 12345) % 0x80000000) / 0x80000000 * (100 - 0 + 1)) + 0);
